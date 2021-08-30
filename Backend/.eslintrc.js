89ed8388fe67c87076a609815e7281c20e5116ba
“Chej <chejahamed@gmail.com>
2021-08-30 11:56:07 +0200

    Add files .env and .env.example

--- Backend/.eslintrc.js
+++ Backend/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "env": {
         "commonjs": true,
         "es2021": true,
-        "node": true
+        "node": true,
+        "jest":true
     },
     "extends": [
         "airbnb-base"
@@ -15,6 +16,6 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {'comma-dangle': [2, 'never'], 'linebreak-style': 0, 'global-require': 0, 'eslint linebreak-style': [0, 'error', 'windows']
-    
+
     }
 };
