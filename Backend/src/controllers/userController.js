8fccb2b129d96ccd327f66481a9c242e18da4ac4
“Chej <chejahamed@gmail.com>
2021-08-31 13:51:51 +0200

    Add getAll in userController

---
+++ Backend/src/controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/userModel');
+
+async function getAll({ query }, res) {
+  try {
+    const users = await User.find(query);
+    res.json(users);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+module.exports = {
+  getAll
+};
